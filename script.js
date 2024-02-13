bfb483d2f81035790847294c1c02f93f0d586507
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-02-13 11:53:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-  document.onmousedown = disableclick;
-  status = "Right Click Disabled";
-  Function disableclick(e)
-  {
-    if(event.button == 2)
-    {
-      alert(status);
-      return false; 
-    }
-  }
+This text copied from site ZealTyro https://blog.zealtyro.com
+document.addEventListener('contextmenu', function(e){
+alert("Sorry, right click is disabled to prevent leakage of confidential functions");
+e.preventDefault();
+});
+Read more: How To Disable 'Inspect Element' And 'View Page Source' Option On Website - ZealTyro https://blog.zealtyro.com/how-to-disable-inspect-element-and-view-page-source-option-on-website
